cc91b3616ad926d6a15bee46a2295a3753efc92b
WendelAmorim <138528392+WendelAmorim@users.noreply.github.com>
2025-03-13 16:47:10 -0300

    Update monopoly.py

--- codigos/monopoly.py
+++ codigos/monopoly.py
@@ -27,9 +27,7 @@ def inserir_dados():
     # Verifica se já existem jogadores e cartas no banco
     if db.jogadores.count_documents({}) == 0:
         jogadores = [
-            {"_id": "player_1", "nome": "Lucas Verde", "saldo": 1500, "posicao": 0, "propriedades": [
-                {"_id": "avenida_atlantica", "nome": "Avenida Atlântica", "cor": "azul", "preco": 400, "aluguel": {"base": 50, "com_1_casa": 200, "com_2_casas": 600, "com_3_casas": 1400, "com_4_casas": 1700, "hotel": 2000}, "casas": 2, "hotel": False, "endereco": "Avenida Atlântica, 1000"}
-            ], "preso": False, "cartoes_sair_da_prisao": 1, "endereco": "Rua das Flores, 123"},
+            {"_id": "player_1", "nome": "Lucas Verde", "saldo": 1500, "posicao": 0, "propriedades": [], "preso": False, "cartoes_sair_da_prisao": 1, "endereco": "Rua das Flores, 123"},
             {"_id": "player_2", "nome": "Ana Souza", "saldo": 1500, "posicao": 0, "propriedades": [], "preso": False, "cartoes_sair_da_prisao": 0, "endereco": "Av. Paulista, 456"},
             {"_id": "player_3", "nome": "Carlos Lima", "saldo": 1500, "posicao": 0, "propriedades": [], "preso": False, "cartoes_sair_da_prisao": 0, "endereco": "Rua do Sol, 789"},
             {"_id": "player_4", "nome": "Mariana Costa", "saldo": 1500, "posicao": 0, "propriedades": [], "preso": False, "cartoes_sair_da_prisao": 0, "endereco": "Av. Brasil, 101"}
